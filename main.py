997d79342b05068ac0e0f8e4d9a86c5dcdb285d7
LukeS-05 <238086390+LukeS-05@users.noreply.github.com>
2026-05-12 20:13:17 +0100

    added more commands and expanded functionality

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
+def writeToFile(tasks):
+    with open("todo.txt", "w") as f:
+        for i in range(0, len(tasks)):
+            f.write(f"{tasks[i]}\n")
+     
+    # main code
 with open("todo.txt", "r") as f:
-    tasks = []
-    for line in f:
-        todo = line.replace("\n", "")
+        tasks = []
+        for line in f:
+            todo = line.replace("\n", "")
+            tasks.append(todo)
+
+while True:
+    print("To do list")
+        
+    for i in range(0, len(tasks)):
+        print(f"{i+1} - {tasks[i]}")
+        
+    command = input()
+    if command.startswith("add"):
+        todo = input()
         tasks.append(todo)
-    
-for i in range(0, len(tasks)):
-    print(f"{i+1} - {tasks[i]}")
+        writeToFile(tasks)
+
+    elif command.startswith("del"):
+        todo = int(input())
+        tasks.remove(tasks[todo-1])
+        writeToFile(tasks)
+        
+    elif command.startswith("exit"):
+        raise SystemExit
