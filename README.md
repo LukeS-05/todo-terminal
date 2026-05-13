49d64f1b886bce20a91ab7f1517babc7dde80687
LukeS-05 <238086390+LukeS-05@users.noreply.github.com>
2026-05-13 17:17:45 +0100

    added input validation checks and help command

---
+++ README.md
@@ -0,0 +1,7 @@
+## ☑️ ToDo Terminal
+⚠️ This project is in early development.
+
+### Features
+🖥️ Open source and easy to inspect.
+🚫 No internet access required
+🔒 No data is collected or stored
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-version = "0.1.1"
+version = "0.1.2"
 
 def writeToFile(tasks):
     with open("todo.txt", "w") as f:
         for i in range(0, len(tasks)):
             f.write(f"{tasks[i]}\n")
-     
+    
 # main code
 with open("todo.txt", "r") as f:
         tasks = []
@@ -19,8 +19,8 @@ while True:
     for i in range(0, len(tasks)):
         print(f"{i+1} - {tasks[i]}")
         
-    command = input()
-    cmdlist = command.split(" | ")
+    command = input(">> ")
+    cmdlist = command.split("|")
     match cmdlist[0]:
         case "add":
             if len(cmdlist) < 2:
@@ -30,22 +30,47 @@ while True:
             tasks.append(todo)
             writeToFile(tasks)
         case "mod":
-            if len(cmdlist) < 2:
+            if len(cmdlist) < 3:
                 print("Invalid arguments - Format - mod | <task number> | <new text>")
                 continue
-            todo = int(cmdlist[1])
+            try:
+                todo = int(cmdlist[1])
+            except ValueError:
+                print("Invalid arguments - arg2 must be an integer")
             newtext = cmdlist[2]
             tasks[todo-1] = newtext
+            writeToFile(tasks)
+        case "swap":
+            if len(cmdlist) < 3:
+                print("Invalid arguments - Format - swap | <task number> | <task number>")
+                continue
+            try:
+                swap1 = int(cmdlist[1]) -1
+                swap2 = int(cmdlist[2]) -1
+            except ValueError:
+                print("Invalid arguments - arg2 and arg3 must be integers")
+            
+            tasks[swap1], tasks[swap2] = tasks[swap2], tasks[swap1]
             writeToFile(tasks)
         case "del":
             if len(cmdlist) < 2:
                 print("Invalid arguments - Format - del | <task number>")
                 continue
-            todo = int(cmdlist[1])
+            try:
+                todo = int(cmdlist[1])
+            except ValueError:
+                print("Invalid arguments - arg2 must be an integer")
             del tasks[todo-1]
             writeToFile(tasks)
         case "clear":
-            tasks = []
-            writeToFile(tasks)
+            confirm = input("Clearing your to-do list is a permanent action.\nType 'clear' to continue.")
+            if confirm == "clear":
+                tasks = []
+                writeToFile(tasks)
+                print("Successfully cleared todo list.")
+            else:
+                print("Cancelled")
+        case "help":
+            print("Coming soon")
         case "exit":
             raise SystemExit
